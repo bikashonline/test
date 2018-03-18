f671f470284ba64dc95f906d64b5af904661080e
bikashonline <37506014+bikashonline@users.noreply.github.com>
2018-03-18 19:28:20 +0530

    Add files via upload
    test

---
+++ src/Controller/Component/TestComponent.php
@@ -0,0 +1,12 @@
+<?php
+namespace Test\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Core\Configure;
+
+class TestComponent extends Component{
+
+  function test(){
+	echo "it's working man !!!";
+  }
+}
